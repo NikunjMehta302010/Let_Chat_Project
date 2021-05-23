acaeb77e937175c23c8cf5ec86692b0d980651a5
NikunjMehta302010 <76863868+NikunjMehta302010@users.noreply.github.com>
2021-05-23 14:13:41 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,58 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD5QW9v6ZYqPUAqBmX6NUgZpSRl2irjof0",
+    authDomain: "let-chat-project.firebaseapp.com",
+    projectId: "let-chat-project",
+    storageBucket: "let-chat-project.appspot.com",
+    messagingSenderId: "472356617477",
+    appId: "1:472356617477:web:c2719d539f53ffd37f4b6c",
+    measurementId: "G-0MJQLR0HKW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+
+    window.location = "kwitter_page.html";
+}
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+    firebase_message_id = childKey;
+    message_data = childData;
+//Start code
+    console.log(firebase_message_id);
+      console.log(message_data);
+      name = message_data['name'];
+      message = message_data['message'];
+    like = message_data['like'];
+    name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+    message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+    span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+
+   row = name_with_tag + message_with_tag +like_button + span_with_tag;       
+   document.getElementById("output").innerHTML += row;
+//End code
+ } });  }); }
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
